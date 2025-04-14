95fa72e2cb6c7866b8c3fd11a70f163bde4580f5
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-04-14 03:54:30 +0200

    EmpData

---
+++ App.Data/Configrution/EmployeeConfg.cs
@@ -0,0 +1,20 @@
+﻿using App.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Data.Configrution
+{
+    internal class EmployeeConfg : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.Property(e => e.Name).IsRequired().HasColumnType("varchar(50)");
+
+        }
+    }
+}
---
+++ App.Data/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Data.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public Decimal Salary { get; set; }
+
+        bool IsDeleted { get; set; }
+        bool IsActive { get; set; }
+
+        
+
+
+    }
+}
